960978ac965976bb4c56a2feaea0cafd0de21cd0
xingobar <gary840227@gmail.com>
2021-03-07 13:48:43 +0800

    refactor: device group 調整
    1. device group 調整

--- router/device.go
+++ router/device.go
@@ -2,15 +2,12 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"healthy-api/controllers"
 )
 
 func DeviceRouter(group *gin.RouterGroup) {
 
-	bloodController := controllers.NewBloodController()
-
 	deviceGroup := group.Group("/:deviceid")
 
-	deviceGroup.GET("/bloods", bloodController.Index)
-	deviceGroup.DELETE("/bloods/:id", bloodController.Delete)
+	bloodGroup := deviceGroup.Group("/bloods")
+	BloodRouter(bloodGroup)
 }
