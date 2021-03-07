5937c5833ada1d2c0a88290c53f5c090408d0328
xingobar <gary840227@gmail.com>
2021-03-07 13:14:38 +0800

    feat: 取得分頁 meta
    1. 取得分頁 meta

--- controllers/blood_controller.go
+++ controllers/blood_controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"healthy-api/models"
 	"net/http"
+	"strconv"
 )
 
 type bloodController struct {
@@ -21,5 +22,20 @@ func (c *bloodController) Index(ctx *gin.Context) {
 		Where("device_id = ?", ctx.Param("deviceid")).
 		Find(&bloods)
 
-	ctx.JSON(http.StatusOK, bloods)
+	total := 0
+
+	models.Db.Model(&models.Blood{}).Where("device_id = ?", ctx.Param("deviceid")).Count(&total)
+
+	page, err := strconv.Atoi(ctx.Request.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": bloods,
+		"meta": map[string]interface{} {
+			"total": total,
+			"page": page,
+		},
+	})
 }
